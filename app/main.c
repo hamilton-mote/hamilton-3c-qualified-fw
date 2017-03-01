52ca2750b47da6c4a8e166bee0bdc32cd6ff3edb
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-03-01 11:04:06 -0800

    Got sensors working. Debugging nrg

--- app/main.c
+++ app/main.c
@@ -14,71 +14,64 @@
 #include <tmp006.h>
 #include <hdc1000.h>
 #include <hdc1000_params.h>
+#include <fxos8700.h>
+
 #include <periph/gpio.h>
 #include <periph/i2c.h>
 #include <periph/adc.h>
 
-#define SAMPLE_INTERVAL ( 10000000UL)
+#define SAMPLE_INTERVAL ( 30000000UL)
+// #define SAMPLE_INTERVAL ( 10000000UL)
 #define SAMPLE_JITTER   ( 2000000UL)
 
-#define MAG_ACC_TYPE_FIELD 5
-#define TEMP_TYPE_FIELD 6
+#define TYPE_FIELD 8
 
 void send_udp(char *addr_str, uint16_t port, uint8_t *data, uint16_t datalen);
 
-uint32_t sample_counter;
+#define AES_SKIP_START_BYTES 4
 
-typedef struct __attribute__((packed)) {
+typedef struct __attribute__((packed,aligned(4))) {
   uint16_t type;
-  int16_t flags; //which of the fields below exist, bit 0 is acc_x
+  uint16_t serial;
+  //From below is encrypted
+  //We use a zero IV, so it is important that the first AES block
+  //is completely unique, which is why we include uptime.
+  //It is expected that hamilton nodes never reboot and that
+  //uptime is strictly monotonic
+  uint64_t uptime;
+  uint16_t flags; //which of the fields below exist
   int16_t acc_x;
   int16_t acc_y;
   int16_t acc_z;
   int16_t mag_x;
   int16_t mag_y;
   int16_t mag_z;
-  uint64_t uptime;
-} mag_acc_measurement_t;
-
-typedef struct __attribute__((packed)) {
-  uint16_t type;
-  uint16_t flags; //which of the fields below exist
   uint16_t tmp_die;
   uint16_t tmp_val;
   int16_t hdc_tmp;
   int16_t hdc_hum;
   uint16_t light_lux;
   uint16_t buttons;
-  uint64_t uptime;
   uint16_t occup;
-} temp_measurement_t;
+} ham7c_t;
 
-void sample_mag(mag_acc_measurement_t *m);
-void sample_temp(temp_measurement_t *m);
+//Uptime is mandatory (for AES security)
+#define FLAG_ACC_X    (1<<0)
+#define FLAG_ACC_Y    (1<<1)
+#define FLAG_ACC_Z    (1<<2)
+#define FLAG_MAG_X    (1<<3)
+#define FLAG_MAG_Y    (1<<4)
+#define FLAG_MAG_Z    (1<<5)
+#define FLAG_TMP_DIE  (1<<6)
+#define FLAG_TMP_VAL  (1<<7)
+#define FLAG_HDC_TMP  (1<<8)
+#define FLAG_HDC_HUM  (1<<9)
+#define FLAG_LUX      (1<<10)
+#define FLAG_BUTTONS  (1<<11)
+#define FLAG_OCCUP    (1<<12)
 
-#define FLAG_MAG_HAS_ACC_X  0x01
-#define FLAG_MAG_HAS_ACC_Y  0x02
-#define FLAG_MAG_HAS_ACC_Z  0x04
-#define FLAG_MAG_HAS_MAG_X  0x08
-#define FLAG_MAG_HAS_MAG_Y  0x10
-#define FLAG_MAG_HAS_MAG_Z  0x20
-#define FLAG_MAG_HAS_UPTIME  0x40
-
-#define FLAG_TEMP_HAS_TMP_DIE  0x01
-#define FLAG_TEMP_HAS_TMP_VAL  0x02
-#define FLAG_TEMP_HAS_HDC_TMP  0x04
-#define FLAG_TEMP_HAS_HDC_HUM  0x08
-#define FLAG_TEMP_HAS_LUX      0x10
-#define FLAG_TEMP_HAS_BUTTONS  0x20
-#define FLAG_TEMP_HAS_UPTIME   0x40
-#define FLAG_TEMP_HAS_OCCUP    0x80
-
-#define FLAGS_COMBO  (FLAG_TEMP_HAS_HDC_TMP |\
-                      FLAG_TEMP_HAS_HDC_HUM |\
-                      FLAG_TEMP_HAS_LUX |\
-                      FLAG_TEMP_HAS_OCCUP |\
-                      FLAG_TEMP_HAS_BUTTONS |\
-                      FLAG_TEMP_HAS_UPTIME)
+//All of the flags
+#define PROVIDED_FLAGS ((1<<13)-1)
 
 //It's actually 6.5*2ms but lets give it 20ms to account for oscillator etc
 #define HDC_ACQUIRE_TIME (20000UL)
@@ -88,6 +81,7 @@ void sample_temp(temp_measurement_t *m);
 
 tmp006_t tmp006;
 hdc1000_t hdc1080;
+fxos8700_t fxos8700;
 
 uint16_t occupancy_events;
 uint16_t button_events;
@@ -96,9 +90,6 @@ bool pir_high;
 uint64_t pir_rise_time;
 uint32_t acc_pir_time;
 uint64_t last_pir_reset;
-
-temp_measurement_t tm __attribute__((aligned(4)));
-mag_acc_measurement_t am __attribute__((aligned(4)));
 
 void critical_error(void) {
   printf("CRITICAL ERROR, REBOOT\n");
@@ -191,19 +182,13 @@ void low_power_init(void) {
 
 }
 
-void sample_mag(mag_acc_measurement_t *m) {
-    m->type = MAG_ACC_TYPE_FIELD;
-  //  m->flags = FLAG_HAS_TEMP;
-    m->flags |= FLAG_MAG_HAS_UPTIME;
-    m->uptime = xtimer_usec_from_ticks64(xtimer_now64());
-}
-
-void sample_temp(temp_measurement_t *m) {
+void sample(ham7c_t *m) {
+    uint8_t drdy;
 
     /* turn on light sensor and let it stabilize */
     gpio_write(GPIO_PIN(0, 28), 0);
 
-    uint8_t drdy;
+
 
     if (tmp006_set_active(&tmp006)) {
         printf("failed to active TMP006\n");
@@ -213,17 +198,16 @@ void sample_temp(temp_measurement_t *m) {
     hdc1000_trigger_conversion(&hdc1080);
 
     /* turn on LED */
-    gpio_write(GPIO_PIN(0, 19), 1);
+  //  gpio_write(GPIO_PIN(0, 19), 1);
 
     xtimer_usleep(HDC_ACQUIRE_TIME);
 
     /* turn off LED */
-    gpio_write(GPIO_PIN(0, 19), 0);
+  //  gpio_write(GPIO_PIN(0, 19), 0);
 
     hdc1000_read(&hdc1080, &m->hdc_tmp, &m->hdc_hum);
-    int adcrv = adc_sample(ADC_PIN_PA08, ADC_RES_16BIT);
-    m->light_lux = (int16_t) adcrv;
-    
+    m->light_lux = (int16_t) adc_sample(ADC_PIN_PA08, ADC_RES_16BIT);
+
     /* Turn off light sensor */
     gpio_write(GPIO_PIN(0, 28), 1);
 
@@ -238,21 +222,39 @@ void sample_temp(temp_measurement_t *m) {
         critical_error();
         return;
     }
-    sample_counter++;
+
+    fxos8700_set_active(&fxos8700);
+    fxos8700_measurement_t fm;
+    if (fxos8700_read(&fxos8700, &fm)) {
+      printf("failed to sample fxos8700\n");
+      critical_error();
+      return;
+    }
+    fxos8700_set_idle(&fxos8700);
+    m->acc_x = fm.acc_x;
+    m->acc_y = fm.acc_y;
+    m->acc_z = fm.acc_z;
+    m->mag_x = fm.mag_x;
+    m->mag_y = fm.mag_y;
+    m->mag_z = fm.mag_z;
     m->uptime = xtimer_usec_from_ticks64(xtimer_now64());
     m->occup = ((uint64_t) acc_pir_time * 32768) / (m->uptime - last_pir_reset);
     last_pir_reset = m->uptime;
     acc_pir_time = 0;
-    m->type = TEMP_TYPE_FIELD;
-    m->flags = FLAGS_COMBO;
+    m->type = TYPE_FIELD;
+    m->flags = PROVIDED_FLAGS;
     m->buttons = button_events;
 
-      printf("drdy %d\n", drdy);
-      printf("sampled lux: %d\n", (int)m->light_lux);
-      printf("sampled temp ok hdct=%d hdch=%d \n", (int)m->hdc_tmp, (int)m->hdc_hum);
-      printf("sampled rad ok tdie=%d tval=%d \n", (int)m->tmp_die, (int)m->tmp_die);
-      printf("sampled PIR %d\n", (int)m->occup);
-      printf("buttone %d\n", (int)m->buttons);
+    int32_t t = (((int32_t)m->tmp_die)*3125)/4000;
+    printf("drdy %d\n", drdy);
+    printf("sampled lux: %d\n", (int)m->light_lux);
+    printf("sampled temp ok hdct=%d hdch=%d \n", (int)m->hdc_tmp, (int)m->hdc_hum);
+    printf("sampled rad ok tdie=%d tval=%d \n", (int)t, (int)m->tmp_val);
+    printf("sampled PIR %d\n", (int)m->occup);
+    xtimer_usleep(100000);
+    printf("buttone %d\n", (int)m->buttons);
+    printf("sampled acc x=%d y=%d z=%d\n", (int)m->acc_x, (int)m->acc_y, (int)m->acc_z);
+    printf("sampled mag x=%d y=%d z=%d\n", (int)m->mag_x, (int)m->mag_y, (int)m->mag_z);
 }
 
 uint32_t interval_with_jitter(void)
@@ -263,7 +265,7 @@ uint32_t interval_with_jitter(void)
     return (uint32_t)t;
 }
 
-
+ham7c_t frontbuf;
 
 int main(void)
 {
@@ -274,11 +276,12 @@ int main(void)
     low_power_init();
     kernel_pid_t radio[GNRC_NETIF_NUMOF];
     uint8_t radio_num = gnrc_netif_get(radio);
+
     while (1) {
       //Sample
-      sample_temp(&tm);
+      sample(&frontbuf);
       //Send
-      send_udp("ff02::1",4747,(uint8_t*)&tm,sizeof(temp_measurement_t));
+      send_udp("ff02::1",4747,(uint8_t*)&frontbuf,sizeof(frontbuf));
       //Radio off
       for (int i=0; i < radio_num; i++)
         gnrc_netapi_set(radio[i], NETOPT_STATE, 0, &radio_state, sizeof(netopt_state_t));
